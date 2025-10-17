76c70497f358133bffe312e07de280702923c347
Uminda H. <68943349+Spyboss@users.noreply.github.com>
2025-10-18 03:18:41 +0530

    Restore conditional static export mode

--- next.config.js
+++ next.config.js
@@ -1,19 +1,25 @@
 /** @type {import('next').NextConfig} */
+const isStaticExport = process.env.NEXT_OUTPUT === 'export';
+
+const images = {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'res.cloudinary.com',
+    },
+    {
+      protocol: 'https',
+      hostname: 'img.shields.io',
+    },
+  ],
+  ...(isStaticExport ? { unoptimized: true } : {}),
+};
+
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'img.shields.io',
-      },
-    ],
-  },
+  images,
   trailingSlash: true,
+  ...(isStaticExport ? { output: 'export' } : {}),
 };
 
 module.exports = nextConfig;
